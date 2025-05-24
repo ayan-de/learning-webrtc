ff75d5e0a7efb2d116f1dc59eb2675c8b2f292d9
Ayan De <deayan252@gmail.com>
2025-05-25 03:19:01 +0530

    feat: createAnswer for the localpeer to send SDP to the remotePeer

--- script.js
+++ script.js
@@ -54,6 +54,51 @@ let createOffer = async () => {
   document.getElementById("offer-sdp").value = JSON.stringify(offer);
 };
 
+let createAnswer = async () => {
+  //creating peer connection
+  peerConnection = new RTCPeerConnection(servers);
+
+  //getting media stream from remoteSteam
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = localStream;
+
+  //adding our all media tracks to the peerconnection
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  //adding that track to the remote stream
+  peerConnection.ontrack = async (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack(track);
+    });
+  };
+
+  //ice candidate
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      document.getElementById("answer-sdp").value = JSON.stringify(
+        peerConnection.localDescription
+      );
+    }
+  };
+
+  let offer = document.getElementById("offer-sdp").value;
+
+  if (!offer) return alert("Get offer from peer first");
+
+  offer = JSON.parse(offer);
+  await peerConnection.setRemoteDescription(offer);
+
+  let answer = await peerConnection.createAnswer();
+  await peerConnection.setLocalDescription(answer);
+
+  document.getElementById("answer-sdp").value = JSON.stringify(answer);
+};
+
 init();
 
 document.getElementById("create-offer").addEventListener("click", createOffer);
+document
+  .getElementById("create-answer")
+  .addEventListener("click", createAnswer);
