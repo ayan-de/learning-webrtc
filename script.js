6469a05c22b0c239089ca9d99436d8d187a44578
Ayan De <deayan252@gmail.com>
2025-05-25 02:36:39 +0530

    feat:

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const constraints = { audio: false, video: true };
 
+let peerConnection;
 let localStream;
 let remoteStream;
 
+//created STUN servers
+let servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"],
+    },
+  ],
+};
+
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia(constraints);
-  remoteStream = new MediaStream();
 
   document.getElementById("user-1").srcObject = localStream;
+};
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = localStream;
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  document.getElementById("offer-sdp").value = JSON.stringify(offer);
 };
 
 init();
+
+document.getElementById("create-offer").addEventListener("click", createOffer);
